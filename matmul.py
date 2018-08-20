e0827ec6567163fbb2de574f080c2538e16cb900
Ryosuke Okuta <okuta@preferred.jp>
2018-08-20 23:47:12 +0900

    Fix matmul

--- matmul.py
+++ matmul.py
@@ -5,9 +5,9 @@ import util
 
 
 for xp in [cupy, numpy]:
-    for size in [2 ** i for i in range(12)]:
-        a = xp.zeros((size, size), dtype='f')
-        b = xp.zeros((size, size), dtype='f')
+    for size in [2 ** i for i in range(11)]:
+        a = xp.zeros((size, 32, 32), dtype='f')
+        b = xp.zeros((size, 32, 32), dtype='f')
         def f():
             xp.matmul(a, b)
         str = "cupy" if xp is cupy else "numpy"
