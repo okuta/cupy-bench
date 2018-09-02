5286f621efe9d57f2a71287027d2c936aecb5e72
Ryosuke Okuta <okuta@preferred.jp>
2018-09-03 01:36:22 +0900

    Add fusion bench

---
+++ fusion.py
@@ -0,0 +1,38 @@
+import cupy
+import numpy
+
+import util
+
+
+def saxpy(a, x, y):
+    return a * x + y
+
+
+@cupy.fuse()
+def saxpy_fuse(a, x, y):
+    return a * x + y
+
+
+for xp in [cupy, numpy]:
+    for size in [2 ** i for i in range(20)]:
+        if xp is numpy and size > 2 ** 13:
+            continue
+        a = numpy.float32(2.0)
+        x = xp.zeros((1024, size), dtype='f')
+        y = xp.zeros((1024, size), dtype='f')
+
+        def f():
+            saxpy(a, x, y)
+        str = "cp" if xp is cupy else "np"
+        util.measure(f, "saxpy_%s  , %8d" % (str, size))
+
+xp = cupy
+for size in [2 ** i for i in range(20)]:
+    a = numpy.float32(2.0)
+    x = xp.zeros((1024, size), dtype='f')
+    y = xp.zeros((1024, size), dtype='f')
+
+    def f():
+        saxpy_fuse(a, x, y)
+    util.measure(f, "saxpy_fuse, %8d" % (size))
+
